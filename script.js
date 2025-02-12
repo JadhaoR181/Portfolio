5d88a374625b4715a453e15786945677cbdb4f6a
Ravindra Jadhav <jadhaor181@gmail.com>
2025-02-13 02:20:11 +0530

    final update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+window.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('.fade-in');
+  sections.forEach((section, index) => {
+      setTimeout(() => {
+          section.style.animation = 'fadeIn 1s forwards';
+      }, index * 300);
+  });
+});
